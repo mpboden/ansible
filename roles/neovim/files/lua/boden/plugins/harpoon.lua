2e2a0d864d803cb10fb193ebbc4989b9f337836d
Mike Boden <mike@mikeboden.com>
2024-08-09 19:06:50 -0700

    Fixed keymap for adding file and viewing menu with telescope

--- roles/neovim/files/lua/boden/plugins/harpoon.lua
+++ roles/neovim/files/lua/boden/plugins/harpoon.lua
@@ -34,7 +34,7 @@ return {
         vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
         { desc = "Open harpoon window" })
 
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 
         vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end)
